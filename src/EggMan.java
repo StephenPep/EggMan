3cac5442ed903f66c6d28aa401bafb717c9144b5
StephenPep <swereko@stetson.edu>
2024-11-02 16:17:20 -0400

    Initial commit

---
+++ src/EggMan.java
@@ -0,0 +1,52 @@
+
+public class EggMan {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!\n");
+		/*
+		 * This prompt is to print the "Hello World"
+		 * command in the console
+		 */
+		System.out.println("This is my first Java Program!!\n");
+		// Printing out the "The is my first Java Program
+		
+		System.out.println("  ________");
+		System.out.println(" /        \\");
+		System.out.println("/          \\");
+		System.out.println("\\          /");
+		System.out.println(" \\________/\n");
+		// Printing the first section of the image
+		
+		System.out.println("\\           /");
+		System.out.println(" \\_________/");
+		System.out.println("+-----------+\n");
+		// Printing the second part of the image
+		
+		System.out.println("  ________");
+		System.out.println(" /        \\");
+		System.out.println("/          \\");
+		System.out.println("|   STOP   |");
+		System.out.println("\\          /");
+		System.out.println(" \\________/\n");
+		// Printing the third section of the image
+		
+		System.out.println("  ________");
+		System.out.println(" /        \\");
+		System.out.println("/          \\");
+		System.out.println("+----------+\n");
+		// Printing the last section of the image 
+		
+		System.out.println("Go Hatters!!\n");
+		/* This is used to print out onto the console
+		 * the "Go Hatters" command
+		 */
+		
+		System.out.println("Program by: Stephen Peprah Wereko");
+		/* This is where my program ends,
+		 * by printing out "Program by:(my name)
+		 */
+		
+	}
+
+}
